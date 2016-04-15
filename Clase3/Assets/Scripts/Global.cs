fa476d9d4d55f6963bd525858ce537ada839c30e
Alumno-J <Alumno-J>
2016-04-14 22:37:05 -0500

    clase3

---
+++ Clase3/Assets/Scripts/Global.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Runner
+{
+	public static class Global
+	{
+		public const string MENU = "Menu";
+		public const string GAME = "Game";
+		public const string PLAYER_SELECT = "PlayerSelection";
+	}
+}
